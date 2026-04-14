7e77b72b598392eaeb9e2560834382e401046e53
qutubuddin45 <qutubuddin011@gmail.com>
2026-04-14 22:51:40 +0530

    initial frontend setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ const App = () => {
+  return (
+    <div class="text-3xl font-bold underline">App
+      <h1 className='text-red-500'>hello</h1>
+    </div>
+    
+  )
+}
+
+export default App
