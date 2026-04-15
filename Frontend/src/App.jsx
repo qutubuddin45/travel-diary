75019fdda4edb0593e4c2484dfa19c1e06e12899
qutubuddin45 <qutubuddin011@gmail.com>
2026-04-15 12:27:52 +0530

    react-router-dom & setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-
+import { BrowserRouter , Route , Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import SignUp from './pages/auth/SignUp'
+import login from './pages/auth/login'
  const App = () => {
   return (
-    <div class="text-3xl font-bold underline">App
-      <h1 className='text-red-500'>hello</h1>
-    </div>
-    
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' exact element={<Home />}></Route>
+    <Route path='/login' exact element={<login/>}></Route>
+    <Route path='/Sign-up' exact element={<SignUp />}></Route>
+   </Routes>
+   </BrowserRouter>
+   
+   
+   
   )
 }
 
