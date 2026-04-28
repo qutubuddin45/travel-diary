7b862f34387b456e851ff0ca9b73fbf992aeeb90
qutubuddin45 <qutubuddin011@gmail.com>
2026-04-28 12:52:33 +0530

    add a middleware a function to handle errors

--- Backend/index.js
+++ Backend/index.js
@@ -22,3 +22,13 @@ app.listen(3000,()=>{
     console.log("server is running on port 3000");
     
 })
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal Server Error"
+    res.status(statusCode).json({
+        success : false,
+        message,
+        statusCode,
+    })
+})
