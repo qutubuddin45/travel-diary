68aa943375c5ed17fc80757c5f5fdb9675938a22
qutubuddin45 <qutubuddin011@gmail.com>
2026-04-28 10:33:01 +0530

    index updated

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
@@ -12,6 +13,10 @@ mongoose.connect(process.env.MONGODB_URL).then(()=>{
 });
 const app = express();
 
+
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);  
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
